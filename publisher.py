ac43af100d36417913e1d0b6d5b80d473d3675ae
Bernese-Corgi <jnyng0621@gmail.com>
2023-02-08 16:31:21 +0900

    Feat: coupon rule 테이블에서 쿠폰번호 가져온 후 해당 쿠폰번호를 메시지로 게시

--- publisher.py
+++ publisher.py
@@ -1,17 +1,29 @@
 from google.cloud import pubsub_v1
 from decouple import config
+import requests
 
 project_id = config('PROJECT_ID')
 topic_id = config('TOPIC_ID')
+danaka_url = config('DANAKA_URL')
+newjoin_rule_type = config('NEWJOIN_ID')
 
 publisher = pubsub_v1.PublisherClient()
 
 topic_path = publisher.topic_path(project_id, topic_id)
 
-for n in range(1, 10):
-  data_str = f"Message number {n}"
-  data = data_str.encode('utf-8')
-  future = publisher.publish(topic_path, data)
-  print(future.result())
+def get_coupon_rule(rule_type):
+  response = requests.get(
+    f"{danaka_url}/coupon/rule",
+    params={ "ruleType": rule_type }
+  )
+  return response.json()
 
-print(f"Published message to {topic_path}.")
+coupon_rule = get_coupon_rule(newjoin_rule_type)
+
+coupon_list = [str(rule["couponNo"]) for rule in coupon_rule]
+data_str = ",".join(coupon_list)
+data = data_str.encode('utf-8')
+
+future = publisher.publish(topic_path, data)
+
+print(future.result())
