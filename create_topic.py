6eba92e4b0cc0cb98cc9bc254c4b085503c11b44
Bernese-Corgi <jnyng0621@gmail.com>
2023-02-08 14:08:30 +0900

    Feat: 토픽 생성

---
+++ create_topic.py
@@ -0,0 +1,12 @@
+from google.cloud import pubsub_v1
+from decouple import config
+
+project_id = config('PROJECT_ID')
+topic_id = "ATT_NEWJOIN"
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(project_id, topic_id)
+
+topic = publisher.create_topic(request={"name": topic_path})
+
+print(f"Created topic: {topic.name}")
