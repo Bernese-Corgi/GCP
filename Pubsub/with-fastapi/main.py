ce6391479d59d124b5943b1118884dcdd524a275
Bernese-Corgi <jnyng0621@gmail.com>
2023-04-25 01:00:16 +0900

    [Pubsub] pub 라우터 추가

--- Pubsub/with-fastapi/main.py
+++ Pubsub/with-fastapi/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 
+from router import pub
+
 
 def start_server():
     app = FastAPI()
     
-    # app.include_router()
-    
+    app.include_router(
+        router=pub.router,
+        prefix="/pub",
+        tags=["publish"],
+    )
+
     return app
 
 app = start_server()
